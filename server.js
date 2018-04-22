4d0c9228feeb7eb3a7bc70a8f98f76d2fa6320fc
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-04-23 07:07:29 +0800

    Add file upload form

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const express = require('express'),
 
 const app = express();
 
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/views/index.html');
+})
 
 app.listen(port, () => {
   console.log("Server is listening on:", port);
