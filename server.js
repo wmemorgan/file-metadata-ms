bca75c8109390c5246ec949d464133f3e5e3fd67
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-04-23 08:42:17 +0800

    Add file upload functionality

--- server.js
+++ server.js
@@ -8,9 +8,22 @@ const app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
+
 app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/views/index.html');
 })
+
+app.post('/uploads', upload.single('fileUpload'), async (req, res) => {
+  const data = req.file;
+  console.log(data);
+  try {
+    console.log("File uploaded successfully")
+    res.send("File uploaded successfully")
+    // res.send({ filename: data.originalname, size: data.size});
+  } catch (err) {
+    res.sendStatus(400);
+  }
+});
 
 app.listen(port, () => {
   console.log("Server is listening on:", port);
